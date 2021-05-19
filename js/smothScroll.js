c0324d1dcdbd47ce8982b6e7ff6f4b6058804fb9
europaplus <basshuntep@mail.ru>
2021-05-19 10:11:40 +0300

    reinit

--- js/smothScroll.js
+++ js/smothScroll.js
@@ -1,11 +1,13 @@
-const smothScrollElems = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
+document.addEventListener('DOMContentLoaded', () => {
+   const smoothScrollElems = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
 
-smothScrollElems.forEach(link => {
-   link.addEventListener('click', (event) => {
-      event.preventDefault();
-      const id = link.getAttribute('href').substring(1);
-      document.getElementById(id).scrollIntoView({
-         behavior: 'smooth'
+   smoothScrollElems.forEach(link => {
+      link.addEventListener('click', (event) => {
+         event.preventDefault();
+         const id = link.getAttribute('href').substring(1);
+         document.getElementById(id).scrollIntoView({
+            behavior: 'smooth'
+         });
       });
-   })
+   });
 });
--- js/tabs.js
+++ js/tabs.js
@@ -1,25 +1,27 @@
-const tabsHandlerElems = document.querySelectorAll('[data-tabs-handler]');
-const tabsFieldElems = document.querySelectorAll('[data-tabs-field]');
-const titleSectionElems = document.querySelectorAll('.section__title');
+document.addEventListener('DOMContentLoaded', () => {
+    const tabsHandlerElems = document.querySelectorAll('[data-tabs-handler]');
+    const tabsFieldElems = document.querySelectorAll('[data-tabs-field]');
+    const titleSectionElems = document.querySelectorAll('.section__title');
 
-for (const tab of tabsHandlerElems) {
-    tab.addEventListener('click', () => {
-        tabsHandlerElems.forEach(item => {
-            if (tab === item) {
-                item.classList.add('design-list__item_active');
-            } else {
-                item.classList.remove('design-list__item_active');
-            }
-        })
-        titleSectionElems.forEach(elem => {
-            elem.classList.toggle('hidden');
+    for (const tab of tabsHandlerElems) {
+        tab.addEventListener('click', () => {
+            tabsHandlerElems.forEach(item => {
+                if (tab === item) {
+                    item.classList.add('design-list__item_active');
+                } else {
+                    item.classList.remove('design-list__item_active');
+                }
+            })
+            titleSectionElems.forEach(elem => {
+                elem.classList.toggle('hidden');
+            });
+            tabsFieldElems.forEach(item => {
+                if (item.dataset.tabsField === tab.dataset.tabsHandler) {
+                    item.classList.remove('hidden');
+                } else {
+                    item.classList.add('hidden');
+                }
+            })
         });
-        tabsFieldElems.forEach(item => {
-            if (item.dataset.tabsField === tab.dataset.tabsHandler) {
-                item.classList.remove('hidden');
-            } else {
-                item.classList.add('hidden');
-            }
-        })
-    });
-}
+    }
+});
